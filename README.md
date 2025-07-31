d552e9b713fb02b33d65939bf472a11bccdbe32f
zxd <shisanlailin@outlook.com>
2025-07-31 18:06:58 +0800

    feat: 项目搭建

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-element-plus-x-shiki
+
+#### 自定义高亮库
---
+++ packages/core/__test__/demo.ts
@@ -0,0 +1,7 @@
+import { describe, expect, it } from 'vitest';
+
+describe('dEFAULT_SESSION_CONFIG', () => {
+  it('test demo', () => {
+    expect(1).toBe(1);
+  });
+});
