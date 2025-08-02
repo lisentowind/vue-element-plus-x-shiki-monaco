16e825f7dcad5c6a28e23258a33d3dd22e886d8e
zxd <shisanlianlin@outlook.com>
2025-08-02 14:49:46 +0800

    refactor(core): 移除 Monaco 组件的单独安装功能
    - 删除了 Monaco 组件的 install.ts 文件
    - 从核心组件导出中移除了 Monaco 和 MonacoWithInstall

--- packages/core/src/components/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as Monaco } from './Monaco/index.vue';
-export { default as MonacoWithInstall } from './Monaco/install';
--- packages/core/types/components/Monaco/install.d.ts
+++
@@ -1,6 +0,0 @@
-import { App } from 'vue';
-import { default as MonacoComponent } from './index.vue';
-declare const Monaco: typeof MonacoComponent & {
-    install: (app: App) => void;
-};
-export default Monaco;
