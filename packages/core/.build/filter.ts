fa945b164992e1bde2b9e006ccc7d7a3067de814
zxd <shisanlailin@outlook.com>
2025-08-07 18:10:02 +0800

    build(core): 更新依赖项并调整组件导出
    - 移除 package.json 中的 peerDependencies
    - 更新 .build/filter.ts 中的外部库配置
    - 优化 components.ts 和 index.ts 中的导入路径和格式
    - 将版本号从 0.1.3 升级到 0.1.4-beta.0

--- packages/core/.build/filter.ts
+++ packages/core/.build/filter.ts
@@ -1,9 +1,9 @@
 export const externalFilter = [
   "vue", // Vue 3 核心库
   "vue/jsx-runtime", // Vue JSX 运行时
-  // "@shikijs/monaco", // Shiki Monaco
-  // "monaco-editor-core", // Monaco 编辑器核心库
-  // "shiki", // Shiki
+  "@shikijs/monaco", // Shiki Monaco
+  "monaco-editor-core", // Monaco 编辑器核心库
+  "shiki", // Shiki
   "react",
   "@types/react",
 ];
